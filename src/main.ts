8aeafaa0114f4ce1b60b93ef4dd07cbc212f146f
AMurkin <amurkin1988@yandex.ru>
2019-09-20 15:01:18 +0400

    Input flags

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import * as github from '@actions/github';
 async function run() {
   try {
     const description_regex = core.getInput('description-regex');
+    const description_regex_flags = core.getInput('description-regex-flags', {required: false});
     core.debug(`Applying ${description_regex} to description string.`);
     const description = github.context!.payload!.pull_request!.body;
     console.log(`PR Description: ${description}`);
 
-    if (!description!.match(new RegExp(description_regex, 'i'))) {
+    if (!description!.match(new RegExp(description_regex, description_regex_flags))) {
         core.setFailed(`Please fix your PR SUMMARY line to match ${description_regex}`);
     } else {
         console.log('PR description contains valid SUMMARY line.');
